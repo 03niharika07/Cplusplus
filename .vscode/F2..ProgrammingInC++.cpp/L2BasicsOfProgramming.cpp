b6f71a75bc7f1bc4371e2cd9f3ed239f95a456ac
03niharika07 <niharika20052004@gmail.com>
2026-04-16 18:46:36 +0530

    Push

---
+++ .vscode/F2..ProgrammingInC++.cpp/L2BasicsOfProgramming.cpp
@@ -0,0 +1,11 @@
+// First C++ Program 
+
+#include<iostream> // preprocessor directive
+using namespace std;
+
+int main(){        // starting point of program
+// int - return_type, main() - function
+
+// operating system (OS) main function ko call krta h
+    return 0;      // successful execution
+}
