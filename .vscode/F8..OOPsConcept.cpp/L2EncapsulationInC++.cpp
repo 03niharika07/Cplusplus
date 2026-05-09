c66e65eb2a30921974e8c8a5fad5aa6741d0dee0
03niharika07 <niharika20052004@gmail.com>
2026-05-09 12:53:10 +0530

    New

---
+++ .vscode/F8..OOPsConcept.cpp/L2EncapsulationInC++.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+using namespace std;
+#include<string>
+
+class Student{
+
+public: 
+    // attributes 
+    int id;
+    string name;
+    int age;
+    int nos;
+
+private:
+    // attribute
+    string gfName;
+    int *gpa;
+
+public:
+    // ctor - Default ctor
+    Student(){
+        cout << "Student Default ctor called" << endl;
+    }
+
+    // Parameterised ctor
+    Student(int id,int age,string name,int nos,float gpa,string gfName){
+        this->id = id;
+        this->age = age;
+        this->name = name;
+        this->nos = nos;
+        this->gpa = new int(gpa);
+        this->gfName = gfName;
+    }
+
+    void study(){
+        cout << "Studying" << endl;
+    }
+
+    void sleep(){
+        cout << "Sleeping" << endl;
+    }
+
+    void bunk(){
+        cout << "Bunking" << endl;
+    }
+
+    ~Student(){
+        cout << "Student Default dtor called" << endl;
+    }
+
+private:
+    // behaviour
+    void chatting(){
+        cout << "Chatting with gf" << endl;
+    }
+};
+
+int main(){
+    Student A(1,13,"Neha",6,9.5,"Namrita");
+    cout << A.name << endl;
+    // cout << A.gfName << endl;  // inaccessible
+    // A.chatting();              // inaccessible
+    A.study();
+    A.bunk();
+    A.sleep();
+    cout << A.name << " " << A.age << " " << A.id << endl;
+    
+    return 0;
+}
