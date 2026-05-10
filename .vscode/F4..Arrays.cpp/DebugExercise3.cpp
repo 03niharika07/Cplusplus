d6358a81d03d9731f6d1c3c57edda272182d269d
03niharika07 <niharika20052004@gmail.com>
2026-05-10 18:25:55 +0530

    New'

--- .vscode/F4..Arrays.cpp/DebugExercise3.cpp
+++ .vscode/F4..Arrays.cpp/DebugExercise3.cpp
@@ -73,12 +73,113 @@
 
 // Debug Solution3 :
 
-void populate(int arr[],int n){
-    int j=0;
-    for(int i=0; i < n; i = i + 2){
-        arr[n-j] = i+2;
-        arr[j] = i+1;
-        j++;
-    }
-}
+// void populate(int arr[],int n){
+//     int j=0;
+//     for(int i=0; i < n; i = i + 2){
+//         arr[n-j-1] = i+2;
+//         arr[j] = i+1;
+//         j++;
+//     }
+// }
 
+
+// Debug Ques4 : 
+
+// void swapAlternate(int arr[],int size){
+//     for(int i=0;i<size-1;i=i+2){
+//         int temp = arr[i];
+//         arr[i+1] = arr[i];
+//         arr[i] = temp;
+//     }
+// }
+
+// Debug Solution4 :
+
+// void swapAlternate(int arr[],int size){
+//     for(int i=0;i<size-1;i=i+1){
+//         int temp = arr[i];
+//         arr[i] = arr[i+1];
+//         arr[i+1] = temp;
+//     }
+// }
+
+// Debug Ques5 : List all pairs in array that sums to X.
+
+// int pairSumToX(int input[], int size, int x){
+// 	int pairs = 0;
+// 	for(int i=0; i<size; i++){
+// 		for(int j=i; j<size; j++){
+// 			if(input[i] + input[j] == x) pairs++;
+// 		}
+// 	}
+// 	return pairs;
+// }
+
+// Debug Solution5 :
+
+// int pairSumToX(int input[], int size, int x){
+// 	int pairs = 0;
+// 	for(int i=0; i<size; i++){
+// 		for(int j=i+1; j<size; j++){
+// 			if(input[i] + input[j] == x) pairs++;
+// 		}
+// 	}
+// 	return pairs;
+// }
+
+
+// Debug Ques6 : List all triplets in array that sums to X.
+
+// int tripletSumToX(int input[], int size, int x){
+// 	int triplets = 0;
+// 	for(int i=0; i<size; i++){
+// 		for(int j=i; j<size; j++){
+// 			for(int k=j; k<size; k++){
+// 				if(input[i] + input[j] + input[k] == x) triplets++;
+// 			}
+// 		}
+// 	}
+// 	return triplets;
+// }
+
+// Debug Solution6 :
+
+// int tripletSumToX(int input[], int size, int x){
+// 	int triplets = 0;
+// 	for(int i=0; i<size; i++){
+// 		for(int j=i+1; j<size; j++){
+// 			for(int k=j+1; k<size; k++){
+// 				if(input[i] + input[j] + input[k] == x) triplets++;
+// 			}
+// 		}
+// 	}
+// 	return triplets;
+// }
+
+
+// Debug Ques7 : Sort 0s and 1s
+
+// void sort0sand1s(int input[], int size){
+// 	int nextZero = 0;
+// 	for(int i=0; i<size; i++){
+// 		if(input[nextZero] == 0){
+// 			int temp = input[nextZero];
+// 			input[nextZero] = input[i];
+// 			input[i] = temp;
+// 		}
+// 	}
+// }
+
+// Debug Solution7 :
+
+// void sort0sand1s(int input[], int size){
+// 	int nextZero = 0;
+// 	for(int i=0; i<size; i++){
+// 		if(input[i] == 0){
+// 			int temp = input[nextZero];
+// 			input[nextZero] = input[i];
+// 			input[i] = temp;
+//             nextZero++;
+// 		}
+// 	}
+// }
