1e62ac72355575e083dcd4e175283edeb79c6e3b
03niharika07 <niharika20052004@gmail.com>
2026-05-10 15:13:37 +0530

    Today

---
+++ .vscode/F8..OOPsConcept.cpp/L3Practice1.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Animal{
+public:
+    // Attributes
+    string name;
+    string color;
+
+    //ctor
+    Animal(string n,string c){
+        cout << "I am inside Animal Class" << endl;
+        this->name = n;
+        this->color = c;
+    }
+
+    // methods
+    void sleep(){
+        cout << name << "Sleeping" << endl;
+    }
+
+    void eat(){
+        cout << name << "Eating" << endl;
+    }
+
+    // Default dtor
+    ~Animal(){
+        cout << "Animal Default dtor called" << endl; 
+    }
+};
+
+class Dog : public Animal{
+public:
+    // Attributes
+    int noOfLegs;
+
+    // ctor
+    Dog(string name,string color,int noOfLegs) : Animal(name,color){
+        cout << "I am inside Dog class" << endl;
+        this->noOfLegs = noOfLegs;
+    }
+
+    // methods 
+    void bark(){
+        cout << name << "Barking" << endl;
+    }
+};
+
+int main(){
+    // When the access specifier is public in derived class 
+    // All accessible
+    Dog D("Chiku","Black",4);
+    cout << D.name << endl;
+    D.bark();
+    D.sleep();
+    D.eat();
+    cout << D.color << endl;
+
+    return 0;
+
+}
