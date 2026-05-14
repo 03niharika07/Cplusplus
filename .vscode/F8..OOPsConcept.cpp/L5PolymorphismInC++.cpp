f432e80b6d9719b68726ab20aa969194a97c8b8c
03niharika07 <niharika20052004@gmail.com>
2026-05-14 19:27:47 +0530

    Add

--- .vscode/F8..OOPsConcept.cpp/L5PolymorphismInC++.cpp
+++ .vscode/F8..OOPsConcept.cpp/L5PolymorphismInC++.cpp
@@ -1,7 +1,10 @@
 #include<iostream>
 using namespace std;
+#include<vector>
 
-// Method Overloading : Overloading occurs when a class contains multiple methods sharing a name but differing in argument count or argument type.
+// METHOD OVERLOADING : Overloading occurs when a class contains multiple methods sharing a name but differing in argument count or argument type.
+
+// 1. FUNCTION OVERLOADING
 
 class Add{
     public:
@@ -13,20 +16,33 @@ class Add{
 
     // x, y, two int addition
     int sum(int x, int y){
+        cout << "Sum of 2 int : " << endl;
         return x+y;
     }
 
     // x, y, z, three int addition
     int sum(int x, int y, int z){
+        cout << "Sum of 3 int : " << endl;
         return x+y+z;
     }
 
     // x, y, double addition
     double sum(double x, double y){
+        cout << "Sum of 2 double : " << endl;
         return x+y;
     }
 
     int sum(int arr[],int size){
+        cout << "Sum of array elements : " << endl;
+        int sum = 0;
+        for(int i=0; i<size; i++){
+            sum = sum + arr[i];
+        }
+        return sum;
+    }
+
+    int sum(vector<int> arr, int size){
+        cout << "Sum of vector array elements : " << endl;
         int sum = 0;
         for(int i=0; i<size; i++){
             sum = sum + arr[i];
@@ -40,10 +56,94 @@ class Add{
     }
 };
 
+// 2. OPERATOR OVERLOADING
+
+class Complex{
+    public:
+
+    // attributes
+    int real;
+    int imag;
+
+    // default ctor
+    // This is used when we create object without giving values
+    Complex(){
+        real = imag = -1;
+    }
+
+    // parameterised ctor
+    // constructor initializer list is used
+    // This is used when we create object with values
+    Complex(int r, int i): real(r),imag(i){};
+
+    void print(){
+        printf("[%d + i%d]\n", this->real, this->imag);
+    }
+
+    // syntax
+    // Ret_type operator <op> (args){
+    //     code
+    //     return<>
+    // }
+
+    Complex operator + (const Complex &B){
+        // this -> A instance
+        Complex temp;
+        temp.real = this->real + B.real;
+        temp.imag = this->imag + B.imag;
+        return temp;
+    }
+
+    Complex operator - (const Complex &B){
+        Complex temp;
+        temp.real = this->real - B.real;
+        temp.imag = this->imag - B.imag;
+        return temp;
+    }
+
+    bool operator == (const Complex &B){
+        if(this->real == B.real && this->imag == B.imag){
+            return true;
+        }
+        return false;
+    }
+ 
+};
+
 int main(){
 
+    // Function Overloading
     Add a;
     cout << a.sum(3,4) << endl;
+    cout << a.sum(3.40 , 6.10) << endl;
+    cout << a.sum(2,3,4) << endl;
+
+    int arr[] = {1,23,3};
+    int size1 = 3;
+    cout << a.sum(arr,size1) << endl;
+
+    vector<int> brr = {1,2,3,4,50};
+    int size2 = 5;
+    cout << a.sum(brr,size2) << endl;
+
+    cout << endl;
+
+    // Operator Overloading
+    Complex c1(5,6);
+    c1.print();
+    Complex c2(3,3);
+    c2.print();
+
+    Complex C = c1 + c2;    // shows error, + operator ko ye nhi pta ki 2 complex number ko kse plus krte h
+    // without operator overloading , error aaega
+    C.print();
+
+    Complex D = c1 - c2;
+    D.print();
+
+    // 2 complex no.s are equal
+    bool E = c1 == c2;
+    cout << E << endl;
 
     return 0;
 }
