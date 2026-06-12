e8984e0815c8051c0117035c856d3bbffa174024
03niharika07 <niharika20052004@gmail.com>
2026-06-12 21:47:47 +0530

    New changes

--- .vscode/F1..LearnC++.cpp/1FirstCppProgram.cpp
+++ .vscode/F1..LearnC++.cpp/1FirstCppProgram.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 // int main() {
-//     cout << "NAMASTE DUNIA" << endl;  // std::cout << "HELLO, WORLD!" << std::endl;
+//     cout << "NAMASTE DUNIA" << endl;  // std::cout << "HELLO, WORLD!" << std::endl; (if namespace std is not used)
 //     cout << "NAMASTE DUNIA" << "\n";
 //     cout << "NAMASTE DUNIA\n";
 //     return 0;
